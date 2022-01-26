c024cfc558073e7fdf13992f7b39f6ec9f0715e9
joaoromaniello <jaromaniello@gmail.com>
2022-01-26 08:37:22 -0300

    FrontEnd improvements

--- processa/processaDel.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../classe/Funcionario.php';
-$id=(int) $_GET['id'];
-$func=new Funcionario(null,null,null,null,null,null,null,null,null,null,null);
-$func->deletar_funcionario($id);
-
-?>
